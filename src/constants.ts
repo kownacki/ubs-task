361b1510b17341dd2ad93130d61b1041eabdf8b2
Michał Kownacki <mk@michalkownacki.pl>
2020-12-04 05:35:00 +0100

    Use https.

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const USER_EMAIL = 'walter.nelson@hussa.rs';
 
-export const CATEGORIES_PATH = 'http://www.mocky.io/v2/5bcdd3942f00002c00c855ba';
-export const COORDINATORS_PATH = 'http://www.mocky.io/v2/5bcdd7992f00006300c855d5';
+export const CATEGORIES_PATH = 'https://www.mocky.io/v2/5bcdd3942f00002c00c855ba';
+export const COORDINATORS_PATH = 'https://www.mocky.io/v2/5bcdd7992f00006300c855d5';
